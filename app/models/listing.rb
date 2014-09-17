530ed690853474697b50ff7a4f28b0e402cf922d
William Brown <contact@imetwill.com>
2014-09-16 21:54:22 -0700

    correction

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg")
-		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+        validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	else
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
 						   :storage => :dropbox,
